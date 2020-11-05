cf4aacce2f08ab7d74bb9285d8caf4609ff82a19
Bhakti Al Akbar <balapa95@gmail.com>
2020-11-06 05:35:01 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+This is some code I use when code in canvas
+
+## Hot Reload
+You need to install live-server https://github.com/tapio/live-server
+Run `live-server`, then make your changes.
---
+++ app.js
@@ -0,0 +1,35 @@
+class App {
+	constructor() {
+		this.canvas = document.getElementById('canvas');
+		this.ctx = this.canvas.getContext('2d');
+
+		this.pixelRatio = window.devicePixelRatio > 1 ? 2 : 1;
+
+		window.addEventListener('resize', this.resize.bind(this), false);
+		this.resize();
+		window.requestAnimationFrame(this.animate.bind(this));
+	}
+
+	resize() {
+		this.stageWidth = document.body.clientWidth;
+		this.stageHeight = document.body.clientHeight;
+
+		this.canvas.width = this.stageWidth;
+		this.canvas.height = this.stageWidth;
+		this.ctx.scale(this.pixelRatio, this.pixelRatio);
+	}
+
+	animate() {
+		window.requestAnimationFrame(this.animate.bind(this));
+
+		this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight);
+
+		// demo indicator
+		this.ctx.fillStyle = 'red';
+		this.ctx.beginPath();
+		this.ctx.arc(this.stageWidth/2, this.stageHeight/2, 50, 0, 2 * Math.PI);
+		this.ctx.fill();
+	}
+}
+
+new App();
